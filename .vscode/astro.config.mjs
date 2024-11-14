ccee5e7d4f4d09280cfd48a0c646b174c196d32f
Iebe Withouck <iebe.withouck@student.howest.be>
2024-11-14 16:14:21 +0100

    added javascript and themes and preact

--- .vscode/astro.config.mjs
+++ .vscode/astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
-
-import preact from "@astrojs/preact";
-
+import preact from '@astrojs/preact';
 // https://astro.build/config
 export default defineConfig({
-  site: "https://tutorial-astro-withouck.netlify.app",
   integrations: [preact()]
 });
